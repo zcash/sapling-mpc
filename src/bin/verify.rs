60069d9f3e3499d1986d4466f558eaaf59afe075
Jack Grigg <jack@z.cash>
2019-02-11 00:47:13 +0000

    Load individual parameter files if the combined file is not present

--- src/bin/verify.rs
+++ src/bin/verify.rs
@@ -1,16 +1,17 @@
 extern crate blake2_rfc;
+extern crate dirs;
 extern crate pairing;
 extern crate phase2;
 extern crate sapling_crypto;
 
 use blake2_rfc::blake2b::Blake2b;
+use phase2::MPCParameters;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{self, BufReader};
+use std::path::PathBuf;
 
-fn main() {
-    let jubjub_params = sapling_crypto::jubjub::JubjubBls12::new();
-
-    let params = File::open("params").unwrap();
+fn load_combined_params(params: PathBuf) -> (MPCParameters, MPCParameters, MPCParameters) {
+    let params = File::open(params).expect("couldn't open combined params file");
     let mut params = BufReader::with_capacity(1024 * 1024, params);
 
     let sapling_spend = phase2::MPCParameters::read(&mut params, true)
@@ -21,6 +22,61 @@ fn main() {
 
     let sprout_joinsplit = phase2::MPCParameters::read(&mut params, true)
         .expect("couldn't deserialize Sprout JoinSplit params");
+
+    (sapling_spend, sapling_output, sprout_joinsplit)
+}
+
+fn load_one(params: PathBuf) -> io::Result<MPCParameters> {
+    println!("Loading {}", params.to_string_lossy());
+    let params = File::open(params)?;
+    let mut params = BufReader::with_capacity(1024 * 1024, params);
+    phase2::MPCParameters::read(&mut params, true)
+}
+
+fn load_individual_params(
+    params_dir: &PathBuf,
+) -> io::Result<(MPCParameters, MPCParameters, MPCParameters)> {
+    let sapling_spend = load_one(params_dir.join("sapling-spend.params"))?;
+    let sapling_output = load_one(params_dir.join("sapling-output.params"))?;
+    let sprout_groth16 = load_one(params_dir.join("sprout-groth16.params"))?;
+    Ok((sapling_spend, sapling_output, sprout_groth16))
+}
+
+fn main() {
+    let jubjub_params = sapling_crypto::jubjub::JubjubBls12::new();
+
+    let combined_params = PathBuf::from("params");
+    let unix_params_dir = dirs::home_dir().map(|path| path.join(".zcash-params"));
+    let win_osx_params_dir = dirs::data_dir().map(|path| path.join("ZcashParams"));
+    let (sapling_spend, sapling_output, sprout_joinsplit) = if combined_params.exists() {
+        load_combined_params(combined_params)
+    } else {
+        match (unix_params_dir, win_osx_params_dir) {
+            (Some(ref params_dir), _) if params_dir.exists() => {
+                match load_individual_params(params_dir) {
+                    Ok(params) => params,
+                    Err(e) => {
+                        println!("Failed to load parameters: {}", e);
+                        return;
+                    }
+                }
+            }
+            (_, Some(ref params_dir)) if params_dir.exists() => {
+                match load_individual_params(params_dir) {
+                    Ok(params) => params,
+                    Err(e) => {
+                        println!("Failed to load parameters: {}", e);
+                        return;
+                    }
+                }
+            }
+            _ => {
+                println!("Cannot locate the Zcash parameters.");
+                println!("Please run zcash-fetch-params or fetch-params.sh to download the parameters, and then re-run this tool.");
+                return;
+            }
+        }
+    };
 
     let sapling_spend_contributions = sapling_spend
         .verify(sapling_crypto::circuit::sapling::Spend {
